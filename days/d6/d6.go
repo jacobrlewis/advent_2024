03d84b75bd3de2872fd804819152a4d01591f6df
Jacob Lewis <jacobrlewis06@gmail.com>
2024-12-07 01:08:54 -0500

    d6 part 2 (optimized! sub .2 seconds with input file cached) (run it twice)

--- days/d6/d6.go
+++ days/d6/d6.go
@@ -99,6 +99,99 @@ func Part1(file *os.File) int {
 	return touched
 }
 
+type path struct {
+	Runes [][]rune
+}
+
+// given the current coord and direction, test if the guard gets stuck in a loop
+func (p path) findLoop(x int, y int, d direction) bool {
+
+	currentDir := d
+
+	// any path longer than the area of the map is a loop
+	maxSteps := len(p.Runes) * len(p.Runes[0])
+	loopCheckSteps := 0
+
+	for {
+
+		j, i := nextCoord(x, y, currentDir)
+
+		if i < 0 || i >= len(p.Runes) || j < 0 || j >= len(p.Runes[i]) {
+			// made it to edge of map, no loop
+			return false
+		}
+
+		if p.Runes[i][j] == '#' {
+			// hitting a wall, turn direction, do not move
+			currentDir = (currentDir + 1) % 4
+			continue
+		}
+
+		// move to next place
+		x, y = j, i
+		loopCheckSteps++
+
+		if loopCheckSteps > maxSteps {
+			return true
+		}
+	}
+}
+
 func Part2(file *os.File) int {
-	return 0
+	bytes, err := io.ReadAll(bufio.NewReader(file))
+
+	if err != nil {
+		panic(err)
+	}
+
+	// 2D array of runes
+	lines := strings.Split(string(bytes), "\n")
+	runes := make([][]rune, len(lines))
+	for i, line := range lines {
+		runes[i] = []rune(line)
+	}
+
+	path := path{runes}
+
+	obstacleCount := 0
+
+	x, y := findGuardStart(lines)
+	runes[y][x] = 'X'
+	currentDir := up
+
+	for {
+		j, i := nextCoord(x, y, currentDir)
+
+		if i < 0 || i >= len(lines) || j < 0 || j >= len(lines[i]) {
+			// made it to edge of map, break
+			break
+		}
+
+		nextChar := runes[i][j]
+
+		if nextChar == '#' {
+			// hitting a wall, turn direction, do not move
+			currentDir = (currentDir + 1) % 4
+			continue
+		}
+
+		if nextChar == '.' {
+			// moving to a new spot
+			runes[i][j] = '#'
+			loopFound := path.findLoop(x, y, currentDir)
+			runes[i][j] = 'X'
+
+			if loopFound {
+				// each location is only tested for loops once, safe to just increment a counter
+				obstacleCount++
+			}
+		}
+		// no need to test if the next square has already been visited
+		// it would have been tested earlier (before moving to it)
+
+		// move to next place
+		x, y = j, i
+	}
+
+	return obstacleCount
 }
---
+++ days/d6/debug.md
@@ -0,0 +1,8 @@
+**SPOILERS**
+
+debug.txt has a solution of 1
+
+If your solution does not test that a loop can be reached, you will get 2. I didn't understand that the obstacle had to be placed before the guard started moving. I thought it could be placed at any point, which made my first attempts too high.
+
+Once I realized this was an assumption made, I realized you never need to test the same coordinate twice.
+
