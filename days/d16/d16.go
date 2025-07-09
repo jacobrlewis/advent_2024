b8fdb5cf3f28e2ab57c94556ce885e873c2435ad
Jacob Lewis <jacobrlewis06@gmail.com>
2025-07-08 23:12:56 -0400

    day 16 part 2 (finally)

--- days/d16/d16.go
+++ days/d16/d16.go
@@ -112,16 +112,116 @@ func Part1(file *os.File) int {
 	}
 
 	// print the paths used
-	for i := range bestScores {
-		for _, num := range bestScores[i] {
-			fmt.Printf("%06d\t", num)
-		}
-		fmt.Println()
-	}
+	// for i := range bestScores {
+	// 	for _, num := range bestScores[i] {
+	// 		fmt.Printf("%06d\t", num)
+	// 	}
+	// 	fmt.Println()
+	// }
 
 	return bestScores[endY][endX]
 }
 
+type Position struct {
+	X  int
+	Y  int
+	DX int
+	DY int
+}
+
 func Part2(file *os.File) int {
-	return 0
+
+	bestFinalScore := Part1(file)
+	file.Seek(0, 0)
+
+	maze := aoc.Get2DRunes(file)
+	minScores := make(map[Position]int)
+
+	var start Position
+
+	endX, endY := 0, 0
+
+	for y := range maze {
+		for x := range maze[y] {
+			if maze[y][x] == 'S' {
+				start = Position{x, y, 1, 0}
+			}
+			if maze[y][x] == 'E' {
+				endX = x
+				endY = y
+				maze[y][x] = 'O'
+			}
+		}
+	}
+
+	var handleLocation func(p Position, score int) bool
+
+	// depth first search to find all possible optimal paths
+	// mark O if you reach the end with the best score
+	handleLocation = func(p Position, score int) bool {
+		isWall := maze[p.Y][p.X] == '#'
+		isNew := minScores[p] == 0
+		isMoreExpensive := minScores[p] < score
+		tooExpensive := score > bestFinalScore
+
+		// early finishes are required to keep runtime down
+		if isWall || (!isNew && isMoreExpensive) || tooExpensive {
+			return false
+		}
+
+		minScores[p] = score
+
+		if p.X == endX && p.Y == endY {
+			// base case: reached end with optimal score
+			return score <= bestFinalScore
+		}
+
+		straightMove := Position{
+			p.X + p.DX,
+			p.Y + p.DY,
+			p.DX,
+			p.DY,
+		}
+
+		right := Position{
+			p.X,
+			p.Y,
+			p.DY * -1,
+			p.DX,
+		}
+
+		left := Position{
+			p.X,
+			p.Y,
+			p.DY,
+			p.DX * -1,
+		}
+
+		straightResult := handleLocation(straightMove, score+1)
+		rightResult := handleLocation(right, score+1000)
+		leftResult := handleLocation(left, score+1000)
+
+		if straightResult || rightResult || leftResult {
+			// one of the branches succeeded, mark O and return result to previous
+			maze[p.Y][p.X] = 'O'
+			return true
+		}
+
+		return false
+	}
+
+	handleLocation(start, 0)
+
+	// count O characters in grid
+	countO := 0
+	for y := range maze {
+		for x := range maze[y] {
+			if maze[y][x] == 'O' {
+				countO += 1
+			}
+		}
+		fmt.Println(string(maze[y]))
+	}
+
+	return countO
 }
